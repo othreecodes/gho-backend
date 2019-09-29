235559080b7c182f87fb6d73facb2ea43fde0a66
Obi Uchea David <daviduchenna@outlook.com>
2019-09-29 20:49:02 +0100

    enable whitenoise

--- gho/config/common.py
+++ gho/config/common.py
@@ -37,6 +37,7 @@ class Common(Configuration):
     # https://docs.djangoproject.com/en/2.0/topics/http/middleware/
     MIDDLEWARE = (
         'django.middleware.security.SecurityMiddleware',
+        'whitenoise.middleware.WhiteNoiseMiddleware',
         'django.contrib.sessions.middleware.SessionMiddleware',
         'django.middleware.common.CommonMiddleware',
         'django.middleware.csrf.CsrfViewMiddleware',
@@ -86,9 +87,14 @@ class Common(Configuration):
         'django.contrib.staticfiles.finders.AppDirectoriesFinder',
     )
 
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
     # Media files
     MEDIA_ROOT = join(os.path.dirname(BASE_DIR), 'media')
     MEDIA_URL = '/media/'
+
+    # Static files (CSS, JavaScript, Images)
+    STATIC_ROOT = os.path.join(ROOT_DIR, 'static')
+    STATIC_URL = '/static/'
 
     TEMPLATES = [
         {
